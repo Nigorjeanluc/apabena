e4184e954ed3c9565816691d56001c3d26f381fb
Ndiramiye <Ndiramiye>
2017-07-10 13:00:37 +0200

    First commit

---
+++ footer.php
@@ -0,0 +1,79 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-lg-4">
+				<div class="widget">
+					<h4>Get in touch with us</h4>
+					<address>
+					<strong>APABENA</strong><br>
+					 Kigali suite room V124, DB 91<br>
+					 Someplace on Earth </address>
+					<p>
+						<i class="fa fa-phone"></i> +250 788 304 817 or +250 788 675 276 <br>
+						<i class="fa fa-envelope-o"></i> apabena2020@htmail.com
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-4 col-lg-4">
+				<div class="widget">
+					<h4>Information</h4>
+					<ul class="link-list">
+						<li><a href="#">Press release</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Career center</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>
+				</div>
+				
+			</div>
+			<div class="col-sm-4 col-lg-4">
+				<div class="widget">
+					<h4>Pages</h4>
+					<ul class="link-list">
+						<li><a href="#">Press release</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Career center</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>
+				</div>
+			</div>
+			<!--<div class="col-sm-3 col-lg-3">
+				<div class="widget">
+					<h4>Newsletter</h4>
+					<p>Fill your email and sign up for monthly newsletter to keep updated</p>
+                    <div class="form-group multiple-form-group input-group">
+                        <input type="email" name="email" class="form-control">
+                        <span class="input-group-btn">
+                            <button type="button" class="btn btn-theme btn-add">Subscribe</button>
+                        </span>
+                    </div>
+				</div>
+			</div>-->
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>&copy; APABENA - All Right Reserved</p>
+                        <div class="credits">
+                            Designed by <a href="https://www.facebook.com/spartacus.amphoteric" target="_blank" rel="nofollow">Eng. Igor Jean-Luc NDIRAMIYE</a>
+                        </div>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer>
